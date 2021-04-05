837f73f30c493021c7b8fde4ff06d767fb0439b8
Isaiah Davis <ID@isaiahs-imac.myfiosgateway.com>
2021-04-05 17:02:46 -0400

    commit tree e11243feb68e1cfc402ac9b331fdbb7e00f02189

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// const cards = ['Lisa', 'Kaitlin', 'Jan']
+// function writeCards(cards){
+//     return (`Thank you, ${cards} for the wonderful surprise gift!`)
+// }
+
+function writeCards(names,event){
+    let newArr = []
+for (let i = 0; i < names.length; i++){
+    newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArr 
+
+}
+function countDown(){
+    let countDown = 0;
+    while (countDown < 11){
+    console.log(countDown++)
+    }
+}
